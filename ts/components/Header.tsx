b14fc5813255f852babdd029b0f5d42c89977bc0
dmon47 <111059ryu@gmail.com>
2020-06-23 16:58:20 +0900

    Update Header

--- ts/components/Header.tsx
+++ ts/components/Header.tsx
@@ -3,6 +3,10 @@ import {Header as HeaderElem} from 'react-native-elements';
 
 interface OwnProps {}
 
-const Header: React.FC<OwnProps> = () => <HeaderElem>aaa</HeaderElem>;
+const Header: React.FC<OwnProps> = () => (
+  <HeaderElem
+    centerComponent={{text: 'PICK', h3: true, h3Style: {color: '#fff'}}}
+  />
+);
 
 export default Header;
