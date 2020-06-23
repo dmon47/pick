c574518933c87844c9600cd16a62e1b4e799219a
dmon47 <111059ryu@gmail.com>
2020-06-23 16:41:40 +0900

    Add Header

---
+++ ts/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Header as HeaderElem} from 'react-native-elements';
+
+interface OwnProps {}
+
+const Header: React.FC<OwnProps> = () => <HeaderElem>aaa</HeaderElem>;
+
+export default Header;
