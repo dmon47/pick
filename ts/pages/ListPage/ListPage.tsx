536685853c23e9dcf49c472a442269fd65a2fe03
dmon47 <111059ryu@gmail.com>
2020-06-23 16:42:14 +0900

    Update ListPage

--- ts/pages/ListPage/ListPage.tsx
+++ ts/pages/ListPage/ListPage.tsx
@@ -1,9 +1,24 @@
 import React, {useState} from 'react';
+import {Card, Text} from 'react-native-elements';
 
 interface OwnProps {}
 
 const ListPage: React.FC<OwnProps> = () => {
-  const [lists, setLists] = useState([]);
+  const [lists, setLists] = useState([
+    {title: 'aaa', description: 'description'},
+    {title: 'aaa', description: 'description'},
+  ]);
+
+  return (
+    <>
+      {lists.map((list) => (
+        <Card key={list.title}>
+          <Text>{list.title}</Text>
+          <Text>{list.description}</Text>
+        </Card>
+      ))}
+    </>
+  );
 };
 
 export default ListPage;
